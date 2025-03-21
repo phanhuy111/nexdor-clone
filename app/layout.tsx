24d103a26f1d21eb83ebf6ffce862b574932eaee
phanhuyjrrr99 <huy.phan@nexlog.tech>
2025-03-21 22:35:52 +0700

    feat: update hub module

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className={inter.className}>
         <I18nProvider>
           {children}
